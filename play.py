eaffe534c6504d6a9ef86a9633a2622e73f4474f
Sj-ackdo <jackjimi@proton.me>
2026-03-12 16:26:19 +0100

    Added argv to general client play file

--- play.py
+++ play.py
@@ -1,4 +1,12 @@
-# import /src/example_file
+from sys import argv
+
+for i in len(argv)-1:
+    if argv[i] == "--name":
+        client_name = argv[i+1]
+    if argv[i] == "--password"
+        server_password = argv[i+1] # password implementation
+
+
 
 if __name__ == "__main__":
     print("Game")
