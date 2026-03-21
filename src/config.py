e5b51d81cf4499c759c0458f8fa065ec352d1ab9
Julian <julianhavik@outlook.com>
2026-03-21 17:42:41 +0100

    config file added for accurate player count in lobby

---
+++ src/config.py
@@ -0,0 +1,4 @@
+bomb_timer = 15 # time until first bomb explodes
+win_timer = 10 # time the game remains open after a winner is declared
+dashing_cooldown = 2 # dash cooldown
+player_amount = 2 # max amount of players for the server
