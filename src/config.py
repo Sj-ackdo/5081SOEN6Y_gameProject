6d6201d4bcb4d8bb6faa9abd326c5a746050849a
neoboxy <rchmeijs@gmail.com>
2026-03-22 16:08:31 +0100

    Walls added and Game explanation in doc

--- src/config.py
+++ src/config.py
@@ -1,5 +1,19 @@
 import pygame
-walls = [pygame.Rect(0, 670, 1280, 50), pygame.Rect(0, 0, 50, 720), pygame.Rect(1230, 0, 50, 720)]
+walls = [pygame.Rect(0, 670, 1280, 50), pygame.Rect(0, 0, 50, 720), pygame.Rect(1230, 0, 50, 720), pygame.Rect(300, 200, 100, 30),
+    pygame.Rect(000, 300, 300, 30),
+    pygame.Rect(400, 300, 400, 30),
+    pygame.Rect(100, 100, 200, 30),
+    pygame.Rect(400, 100, 400, 30),
+    pygame.Rect(900, 300, 400, 30),
+    pygame.Rect(900, 100, 400, 30),
+    pygame.Rect(800, 200, 100, 30),
+    pygame.Rect(300, 400, 100, 30),
+    pygame.Rect(100, 500, 200, 30),
+    pygame.Rect(400, 500, 400, 30),
+    pygame.Rect(900, 500, 400, 30),
+    pygame.Rect(800, 400, 100, 30),
+    pygame.Rect(300, 600, 100, 30),
+    pygame.Rect(800, 600, 100, 30)]
 bomb_timer = 15 # time until first bomb explodes
 win_timer = 10 # time the game remains open after a winner is declared
 dashing_cooldown = 2 # dash cooldown
